5db9bd733be00668cc14597abfe4ce63571a0602
maxbachmann <kontakt@maxbachmann.de>
2019-03-11 23:05:04 +0100

    comments

--- mqtt-mm2-gateway.js
+++ mqtt-mm2-gateway.js
@@ -1,5 +1,12 @@
 "use strict";
 /**
+ * @file mqtt-mm2-gateway.js
+ * @author Max Bachmann <https://github.com/maxbachmann>
+ * @version 0.1
+ * @see https://github.com/maxbachmann-magicmirror2/mqtt-mm2-gateway.git
+ */
+
+/**
  * @license
  * Copyright (C) 2019 Max Bachmann
  * This program is free software: you can redistribute it and/or modify it
@@ -11,14 +18,42 @@
  * You should have received a copy of the GNU General Public License along with 
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
- /**
- * @file gateway for mqtt messages and mm2 notifications
- * @author Max Bachmann <https://github.com/maxbachmann>
- * @version 0.1
+
+/* global Module Log MM */
+
+/**
+ * @external Module
+ * @see https://github.com/MichMich/MagicMirror/blob/master/js/module.js
  */
 
+/**
+ * @external Log
+ * @see https://github.com/MichMich/MagicMirror/blob/master/js/logger.js
+ */
+
+/**
+ * @external MM
+ * @see https://github.com/MichMich/MagicMirror/blob/master/js/main.js
+ */
+
+/**
+ * @module mqtt-mm2-gateway
+ * @description Gateway for mqtt and mm2 messages
+ *
+ * @requires external:Module
+ * @requires external:Log
+ * @requires external:MM
+ */
 Module.register("mqtt-mm2-gateway", {
 
+  /**
+   * @member {Object} defaults - Defines the default config values.
+   * @property {string} host - host of the MQTT broker (domain or IPv4)
+   * @property {int} port - Port of the MQTT broker
+   * @property {array of strings} topics - Topics to subscribe
+   * @property {string} username - username of the MQTT broker
+   * @property {string} password - password of the MQTT broker
+   */
   defaults: {
     host: 'localhost',
     port: 1883,
@@ -27,40 +62,80 @@ Module.register("mqtt-mm2-gateway", {
     password: "",
   },
 
+  /**
+   * @member {Object} options - MQTT options
+   */
+  options: {},
+
+  /**
+   * @function start
+   * @description Sets mode to initialising.
+   * @override
+   */
   start() {
     Log.info("Starting module: " + this.name);
     this.start_MQTT_client();
   },
 
+  /**
+   * @function username_is_set
+   * @description check wether username is set in the config
+   * @override
+   *
+   * @returns bool is Username set 
+   */
   username_is_set(){
     return this.config.username !== "";
   },
 
+  /**
+   * @function start_MQTT_client
+   * @description send the node_helper a message to start the mqtt client,
+   * with the config options host, port, topics to subsribe to, username
+   * and password
+   * @override
+   */
   start_MQTT_client(){
-    let options = {
-      host: this.config.host,
-      port: this.config.port,
-    };
-    // username + password authentification
+    options.host = this.config.host;
+    options.port = this.config.port;
+  
     if (this.username_is_set()){
         options.username = this.config.username; 
         options.password = this.config.password;
     }
-    this.sendSocketNotification("MQTT_RECEIVE", { 
-      options,
-      topics : this.config.topics
+    this.sendSocketNotification("MQTT_INIT", { 
+      options: options,
+      topics: this.config.topics
     });
   },
 
+  /**
+   * @function notificationReceived
+   * @description Handles incoming broadcasts from other modules or the MagicMirror core.
+   * @override
+   *
+   * @param {string} notification - Notification name
+   * @param {*} payload - Detailed payload of the notification.
+   */
   NotificationReceived(notification, payload) {
     if (notification === "MQTT_SEND") {
-      this.sendSocketNotification("SEND", payload);
+      this.sendSocketNotification("SEND", {
+        options,
+        payload
+      });
     }
   },
 
+  /**
+   * @function notificationReceived
+   * @description Handles incoming broadcasts from node_helper
+   * @override
+   *
+   * @param {string} notification - Notification name
+   * @param {*} payload - Detailed payload of the notification.
+   */
   socketNotificationReceived(notification, payload) {
     if (notification === "MM2_SEND") {
-      this.sendNotification("SHOW_ALERT", payload);
       this.sendNotification(payload.topic, payload.data);
     }else if (notification === "ERROR") {
       this.sendNotification("SHOW_ALERT", payload);
--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,12 @@
 "use strict";
 /**
+ * @file node_helper.js
+ * @author Max Bachmann <https://github.com/maxbachmann>
+ * @version 0.1
+ * @see https://github.com/maxbachmann-magicmirror2/mqtt-mm2-gateway.git
+ */
+
+/**
  * @license
  * Copyright (C) 2019 Max Bachmann
  * This program is free software: you can redistribute it and/or modify it
@@ -11,37 +18,65 @@
  * You should have received a copy of the GNU General Public License along with 
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
- /**
- * @file gateway for mqtt messages and mm2 notifications
- * @author Max Bachmann <https://github.com/maxbachmann>
- * @version 0.1
- */
 
-const NodeHelper = require("node_helper");
+/**
+ * @external node_helper
+ * @see https://github.com/MichMich/MagicMirror/blob/master/modules/node_modules/node_helper/index.js
+ */
+const NodeHelper = require('node_helper');
+
+/**
+ * @external mqtt
+ * @see https://github.com/mqttjs/MQTT.js
+ */
 var mqtt = require("mqtt");
 
+/**
+ * @module node_helper
+ * @description Backend for the module that acts as MQTT client
+ *
+ * @requires external:node_helper
+ * @requires external:mqtt
+ */
 module.exports = NodeHelper.create({
+
+  /**
+   * @function start
+   * @description Logs a start message to the console.
+   * @override
+   */
   start() {
     console.log("MMM-mqtt started ...");
     this.clients = [];
   },
 
-  getMqtt(notification, payload) {
+  /**
+   * @function getMQTT
+   * @description connect to a mqtt broker with the config parameters from the config
+   * @override
+   *
+   * @param {*} notification 
+   * @param {*} config 
+   */
+  getMQTT(notification, config) {
     var self = this;
-    var client = mqtt.connect(payload.options);
+    var client = mqtt.connect(config.options);
 
+    // send to message or subscribe to topics on connect
     client.on("connect", function() {
-      if (payload.notification === "MQTT_SEND"){
-        console.log("Publishing: " + JSON.stringify(payload.message));
-        client.publish(payload.topic, JSON.stringify(payload.message));
+      console.log("Connected to the MQTT broker");
+      if (notification === "MQTT_SEND"){
+        console.log("Publishing: " + JSON.stringify(config.message));
+        client.publish(config.topic, JSON.stringify(config.message));
       } else {
-        for (var i = 0; i < payload.topics.length; ++i) {
-          console.log("Subscribing to topics: " + payload.topics.toString());
-          client.subscribe(payload.topics[i]);
+        for (var i = 0; i < config.topics.length; ++i) {
+          console.log("Subscribing to topics: " + config.topics.toString());
+          client.subscribe(config.topics[i]);
         }
       }
     });
 
+    // report error
     client.on("error", function(error) {
       console.log("*** MQTT ERROR ***: " + error);
       self.sendSocketNotification("ERROR", {
@@ -51,6 +86,7 @@ module.exports = NodeHelper.create({
       });
     });
 
+    //report offline
     client.on("offline", function() {
       console.log("*** MQTT Client Offline ***");
       self.sendSocketNotification("ERROR", {
@@ -60,13 +96,22 @@ module.exports = NodeHelper.create({
       });
     });
 
+    //forward messages
     client.on("message", function(topic, message) {
-      console.log("Recevied: " + message.toString());
+      console.log("Received: " + message.toString());
       self.sendSocketNotification("MM2_SEND", {topic, "message": message.toString()});
     });
   },
 
+  /**
+   * @function socketNotificationReceived
+   * @description Handles incoming broadcasts from the module
+   * @override
+   *
+   * @param {*} notification 
+   * @param {*} payload 
+   */
   socketNotificationReceived(notification, payload) {
-    this.getMqtt(notification, payload);
+    this.getMQTT(notification, payload);
   }
 });
